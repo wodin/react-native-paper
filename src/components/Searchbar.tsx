24c6d2d2c8ca15e66256babdcbfc276a2601efa9
lukewalczak <lukasz.walczak.pwr@gmail.com>
2023-01-09 11:10:33 +0100

    refactor: adjust searchbar to the latest md guidelines

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -18,20 +18,17 @@ import color from 'color';
 import { withInternalTheme } from '../core/theming';
 import type { InternalTheme } from '../types';
 import ActivityIndicator from './ActivityIndicator';
+import Divider from './Divider';
 import type { IconSource } from './Icon';
 import IconButton from './IconButton/IconButton';
 import MaterialCommunityIcon from './MaterialCommunityIcon';
 import Surface from './Surface';
 
+interface Style {
+  marginRight: number;
+}
+
 export type Props = React.ComponentPropsWithRef<typeof TextInput> & {
-  /**
-   * Accessibility label for the button. This is read by the screen reader when the user taps the button.
-   */
-  clearAccessibilityLabel?: string;
-  /**
-   * Accessibility label for the button. This is read by the screen reader when the user taps the button.
-   */
-  searchAccessibilityLabel?: string;
   /**
    * Hint text shown when the input is empty.
    */
@@ -41,17 +38,75 @@ export type Props = React.ComponentPropsWithRef<typeof TextInput> & {
    */
   value: string;
   /**
-   * Icon name for the left icon button (see `onIconPress`).
-   */
-  icon?: IconSource;
-  /**
    * Callback that is called when the text input's text changes.
    */
   onChangeText?: (query: string) => void;
   /**
+   * @supported Available in v5.x with theme version 3
+   * Search layout mode, the default value is "bar".
+   */
+  mode?: 'bar' | 'view';
+  /**
+   * Icon name for the left icon button (see `onIconPress`).
+   */
+  icon?: IconSource;
+  /**
+   * Custom color for icon, default will be derived from theme
+   */
+  iconColor?: string;
+  /**
    * Callback to execute if we want the left icon to act as button.
    */
   onIconPress?: (e: GestureResponderEvent) => void;
+  /**
+   * Accessibility label for the button. This is read by the screen reader when the user taps the button.
+   */
+  searchAccessibilityLabel?: string;
+  /**
+   * Custom icon for clear button, default will be icon close. It's visible when `loading` is set to `false`.
+   * In v5.x with theme version 3, `clearIcon` is visible only `right` prop is not defined.
+   */
+  clearIcon?: IconSource;
+  /**
+   * Accessibility label for the button. This is read by the screen reader when the user taps the button.
+   */
+  clearAccessibilityLabel?: string;
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Icon name for the right trailering icon button.
+   * Works only when `mode` is set to "bar". It won't be displayed if `loading` is set to `true`.
+   */
+  traileringIcon?: IconSource;
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Custom color for the right trailering icon, default will be derived from theme
+   */
+  traileringIconColor?: string;
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Callback to execute on the right trailering icon button press.
+   */
+  onTraileringIconPress?: (e: GestureResponderEvent) => void;
+  /**
+   * Accessibility label for the right trailering icon button. This is read by the screen reader when the user taps the button.
+   */
+  traileringIconAccessibilityLabel?: string;
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Callback which returns a React element to display on the right side.
+   * Works only when `mode` is set to "bar".
+   */
+  right?: (props: {
+    color: string;
+    style: Style;
+    testID: string;
+  }) => React.ReactNode;
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Whether to show `Divider` at the bottom of the search.
+   * Works only when `mode` is set to "view". True by default.
+   */
+  showDivider?: boolean;
   /**
    * @supported Available in v5.x with theme version 3
    * Changes Searchbar shadow and background on iOS and Android.
@@ -62,14 +117,6 @@ export type Props = React.ComponentPropsWithRef<typeof TextInput> & {
    */
   inputStyle?: StyleProp<TextStyle>;
   style?: StyleProp<ViewStyle>;
-  /**
-   * Custom color for icon, default will be derived from theme
-   */
-  iconColor?: string;
-  /**
-   * Custom icon for clear button, default will be icon close
-   */
-  clearIcon?: IconSource;
   /**
    * Custom flag for replacing clear button with activity indicator.
    */
@@ -122,15 +169,22 @@ type TextInputHandles = Pick<
 const Searchbar = React.forwardRef<TextInputHandles, Props>(
   (
     {
-      clearAccessibilityLabel = 'clear',
-      clearIcon,
       icon,
       iconColor: customIconColor,
-      inputStyle,
       onIconPress,
-      placeholder,
       searchAccessibilityLabel = 'search',
-      elevation = 1,
+      clearIcon,
+      clearAccessibilityLabel = 'clear',
+      traileringIcon,
+      traileringIconColor,
+      traileringIconAccessibilityLabel,
+      onTraileringIconPress,
+      right,
+      mode = 'bar',
+      showDivider = true,
+      inputStyle,
+      placeholder,
+      elevation = 0,
       style,
       theme,
       value,
@@ -174,20 +228,37 @@ const Searchbar = React.forwardRef<TextInputHandles, Props>(
     };
 
     const { colors, roundness, dark, isV3 } = theme;
-    const textColor = isV3 ? theme.colors.onSurface : theme.colors.text;
+    const placeholderTextColor = theme.isV3
+      ? theme.colors.onSurface
+      : theme.colors?.placeholder;
+
+    const textColor = isV3 ? theme.colors.onSurfaceVariant : theme.colors.text;
+
+    const md2IconColor = dark
+      ? textColor
+      : color(textColor).alpha(0.54).rgb().string();
     const iconColor =
-      customIconColor ||
-      (dark ? textColor : color(textColor).alpha(0.54).rgb().string());
+      customIconColor || (isV3 ? theme.colors.onSurfaceVariant : md2IconColor);
+
     const rippleColor = color(textColor).alpha(0.32).rgb().string();
+
+    const font = theme.isV3 ? theme.fonts.bodyLarge : theme.fonts.regular;
+
+    const isBarMode = isV3 && mode === 'bar';
 
     return (
       <Surface
         style={[
           { borderRadius: roundness },
           !isV3 && styles.elevation,
+          isV3 && {
+            backgroundColor: theme.colors.elevation.level3,
+            borderRadius: roundness * (isBarMode ? 7 : 0),
+          },
           styles.container,
           style,
         ]}
+        testID={`${testID}-container`}
         {...(theme.isV3 && { elevation })}
       >
         <IconButton
@@ -214,15 +285,14 @@ const Searchbar = React.forwardRef<TextInputHandles, Props>(
             styles.input,
             {
               color: textColor,
-              ...(theme.isV3 ? theme.fonts.default : theme.fonts.regular),
+              ...font,
               ...Platform.select({ web: { outline: 'none' } }),
             },
+            isV3 && (isBarMode ? styles.barModeInput : styles.viewModeInput),
             inputStyle,
           ]}
           placeholder={placeholder || ''}
-          placeholderTextColor={
-            theme.isV3 ? theme.colors.onSurface : theme.colors?.placeholder
-          }
+          placeholderTextColor={placeholderTextColor}
           selectionColor={colors?.primary}
           underlineColorAndroid="transparent"
           returnKeyType="search"
@@ -236,7 +306,7 @@ const Searchbar = React.forwardRef<TextInputHandles, Props>(
         {loading ? (
           <ActivityIndicator
             testID="activity-indicator"
-            style={styles.loader}
+            style={isV3 ? styles.v3Loader : styles.loader}
           />
         ) : (
           // Clear icon should be always rendered within Searchbar – it's transparent,
@@ -246,6 +316,10 @@ const Searchbar = React.forwardRef<TextInputHandles, Props>(
           <View
             pointerEvents={value ? 'auto' : 'none'}
             testID={`${testID}-icon-wrapper`}
+            style={[
+              isV3 && !value && styles.v3ClearIcon,
+              isV3 && right !== undefined && styles.v3ClearIconHidden,
+            ]}
           >
             <IconButton
               borderless
@@ -257,7 +331,7 @@ const Searchbar = React.forwardRef<TextInputHandles, Props>(
                 clearIcon ||
                 (({ size, color }) => (
                   <MaterialCommunityIcon
-                    name="close"
+                    name={isV3 ? 'close' : 'close-circle-outline'}
                     color={color}
                     size={size}
                     direction={I18nManager.getConstants().isRTL ? 'rtl' : 'ltr'}
@@ -267,6 +341,34 @@ const Searchbar = React.forwardRef<TextInputHandles, Props>(
               accessibilityRole="button"
             />
           </View>
+        )}
+        {isBarMode &&
+        traileringIcon &&
+        !loading &&
+        (!value || right !== undefined) ? (
+          <IconButton
+            accessibilityRole="button"
+            borderless
+            onPress={onTraileringIconPress}
+            iconColor={traileringIconColor || theme.colors.onSurfaceVariant}
+            icon={traileringIcon}
+            accessibilityLabel={traileringIconAccessibilityLabel}
+            testID={`${testID}-trailering-icon`}
+          />
+        ) : null}
+        {isBarMode &&
+          right?.({ color: textColor, style: styles.rightStyle, testID })}
+        {isV3 && !isBarMode && showDivider && (
+          <Divider
+            bold
+            style={[
+              styles.divider,
+              {
+                backgroundColor: theme.colors.outline,
+              },
+            ]}
+            testID={`${testID}-divider`}
+          />
         )}
       </Surface>
     );
@@ -286,11 +388,38 @@ const styles = StyleSheet.create({
     textAlign: I18nManager.getConstants().isRTL ? 'right' : 'left',
     minWidth: 0,
   },
+  barModeInput: {
+    paddingLeft: 0,
+    minHeight: 56,
+  },
+  viewModeInput: {
+    paddingLeft: 0,
+    minHeight: 72,
+  },
   elevation: {
     elevation: 4,
   },
   loader: {
     margin: 10,
+  },
+  v3Loader: {
+    marginHorizontal: 16,
+  },
+  rightStyle: {
+    marginRight: 16,
+  },
+  v3ClearIcon: {
+    position: 'absolute',
+    right: 0,
+    marginLeft: 16,
+  },
+  v3ClearIconHidden: {
+    display: 'none',
+  },
+  divider: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
   },
 });
 
--- src/components/__tests__/Searchbar.test.js
+++ src/components/__tests__/Searchbar.test.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import renderer from 'react-test-renderer';
 
+import * as Avatar from '../Avatar/Avatar.tsx';
 import Searchbar from '../Searchbar.tsx';
 
 it('renders with placeholder', () => {
@@ -63,4 +64,91 @@ it('renders clear icon wrapper, which is never target of touch events, if search
   expect(getByTestId('search-bar-icon-wrapper').props.pointerEvents).toBe(
     'none'
   );
+});
+
+it('renders clear icon wrapper, with appropriate style for v3', () => {
+  const { getByTestId, update } = render(
+    <Searchbar testID="search-bar" value="" />
+  );
+
+  expect(getByTestId('search-bar-icon-wrapper')).toHaveStyle({
+    position: 'absolute',
+    right: 0,
+    marginLeft: 16,
+  });
+
+  update(
+    <Searchbar
+      testID="search-bar"
+      value=""
+      right={() => <Avatar.Text label="AB" size={30} />}
+    />
+  );
+
+  expect(getByTestId('search-bar-icon-wrapper')).toHaveStyle({
+    display: 'none',
+  });
+});
+
+it('renders trailering icon when mode is set to "bar"', () => {
+  const { getByTestId } = render(
+    <Searchbar
+      testID="search-bar"
+      value={''}
+      traileringIcon={'microphone'}
+      mode="bar"
+    />
+  );
+
+  expect(getByTestId('search-bar-trailering-icon')).toBeTruthy();
+});
+
+it('renders trailering icon with press functionality', () => {
+  const onTraileringIconPressMock = jest.fn();
+
+  const { getByTestId } = render(
+    <Searchbar
+      testID="search-bar"
+      value={''}
+      traileringIcon={'microphone'}
+      onTraileringIconPress={onTraileringIconPressMock}
+      mode="bar"
+    />
+  );
+
+  fireEvent(getByTestId('search-bar-trailering-icon'), 'onPress');
+  expect(onTraileringIconPressMock).toHaveBeenCalledTimes(1);
+});
+
+it('renders clear icon instead of trailering icon', () => {
+  const { getByTestId, update, queryByTestId } = render(
+    <Searchbar
+      testID="search-bar"
+      value={''}
+      traileringIcon={'microphone'}
+      mode="bar"
+    />
+  );
+
+  expect(getByTestId('search-bar-trailering-icon')).toBeTruthy();
+
+  update(
+    <Searchbar
+      testID="search-bar"
+      value={'test'}
+      traileringIcon={'microphone'}
+      mode="bar"
+    />
+  );
+
+  expect(queryByTestId('search-bar-trailering-icon')).toBeNull();
+  expect(getByTestId('search-bar-icon-wrapper')).toBeTruthy();
+});
+
+it('renders searchbar in "view" mode', () => {
+  const { getByTestId } = render(
+    <Searchbar testID="search-bar" value={''} mode="view" />
+  );
+
+  expect(getByTestId('search-bar-container')).toHaveStyle({ borderRadius: 0 });
 });
